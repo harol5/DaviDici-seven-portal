8f53e3c020a25555246ee6396c73be762e757f01
harol rojas <hrcode95@gmail.com>
2025-01-28 10:32:09 -0500

    errors

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -78,6 +78,7 @@ class OrdersController extends Controller
 
         // sorts orders by date
         if (count($orders['rows']) > 1) {
+            info($orders['rows']);
             usort($orders, function ($a, $b) {
                 return strcmp($b['orderdate'], $a['orderdate']);
             });
