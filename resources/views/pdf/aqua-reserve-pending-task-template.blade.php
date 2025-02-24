0b1ba8ba5680de254a9709148c4bf20e09ab891f
harol rojas <hrcode95@gmail.com>
2025-02-24 14:11:02 -0500

    testing size of images signature

--- resources/views/pdf/aqua-reserve-pending-task-template.blade.php
+++ resources/views/pdf/aqua-reserve-pending-task-template.blade.php
@@ -81,6 +81,10 @@
             text-align: center;
         }
 
+        .content .signatures-container .signature img {
+            width: 80%;
+        }
+
         @page {
             size: letter;
         }
